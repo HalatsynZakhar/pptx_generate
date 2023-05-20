3cdfa80f5a0ad32c6b000c3cf6ad8664357061bf
halatsynzakhar <halatsynzakhar@gmail.com>
2023-05-20 16:50:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+A simple program for generating presentations with lyrics and translation of a song.
+
+
+Input data: title, artist, original text, translation (optional)
+
+Output data0: Power Point presentation. Each line with translation in a separate slide.
---
+++ main.py
@@ -0,0 +1,75 @@
+from pptx import Presentation
+from pptx.dml.color import RGBColor
+from pptx.util import Pt
+from googletrans import Translator
+
+if_exist_translate = False
+inpSinger = "Battle Beast"
+inpTitle = "Eden"
+
+prs = Presentation()
+
+bullet_slide_layout = prs.slide_layouts[0]
+
+with open('input_orig.txt', 'r', encoding="utf-8") as f:
+  input_orig = f.read()
+
+text_orig_list = input_orig.split("\n")
+
+if if_exist_translate:
+    with open('input_trans.txt', 'r', encoding="utf-8") as f:
+        input_trans = f.read()
+    text_trans_list = input_trans.split("\n")
+
+else:
+    translator = Translator()
+    text_trans_list = []
+    for i in range(len(text_orig_list)):
+        text = text_orig_list[i]
+        translation = translator.translate(text, dest="ru")
+        text_trans_list.append(translation.text)
+
+if len(text_trans_list)!=len(text_orig_list):
+    print("Несовпадает количество строк оригинала и перевода. Невозможно сопоставить.")
+    exit()
+
+for i in range(len(text_orig_list)):
+    slide = prs.slides.add_slide(bullet_slide_layout)
+
+    slide.background.fill.solid()
+    slide.background.fill.fore_color.rgb = RGBColor(0, 0, 0)
+
+    shapes = slide.shapes
+
+    title_shape = shapes.title
+    body_shape = shapes.placeholders[1]
+
+    text_frame = slide.shapes[0].text_frame
+
+    text_frame.text = text_orig_list[i] # текст оригинала
+    text_frame.paragraphs[0].font.name = 'TimesNewRoman'
+
+    line = len(text_orig_list[i])
+    if line<=10:
+        font_size = 90
+    else:
+        font_size = round(900/line)
+
+
+    text_frame.paragraphs[0].font.size = Pt(font_size)
+    text_frame.paragraphs[0].font.color.rgb = RGBColor(255, 255, 255)
+
+    tf = body_shape.text_frame
+    tf.text = text_trans_list[i] # текст перевода
+
+    line = len(text_trans_list[i])
+    if line<=10:
+        font_size = 90
+    else:
+        font_size = round(900/line)
+
+    tf.paragraphs[0].font.name = 'TimesNewRoman'
+    tf.paragraphs[0].font.size = Pt(font_size)
+    tf.paragraphs[0].font.color.rgb = RGBColor(255, 255, 255)
+
+prs.save(r"C:\Users\halat\Desktop\songs_pptx\{} - {}.pptx".format(inpSinger, inpTitle))
